b28bed0de7ac2aa42a89c9624128a3c1d80c8dd7
Jimmy Lee <jimmy.lee@careline.com.tw jimmy.lee@careline.com.tw jimmy.lee@careline.com.tw jimmy.lee@careline.com.twjimmy.lee@careline.com.tw>
2017-10-27 09:48:00 +0800

    add local module ( app-layout)

---
+++ shared-layout/src/shared/layout.module.ts
@@ -0,0 +1,17 @@
+
+
+
+import { NgModule } from '@angular/core';
+import { LayoutComponent } from "./layout.component";
+
+@NgModule({
+    imports: [],
+    exports: [
+        LayoutComponent
+    ],
+    declarations: [
+        LayoutComponent
+    ],
+    providers: [],
+})
+export class LayoutModule { }
--- shopping-cart/src/app/app.module.ts
+++ shopping-cart/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { LayoutModule } from 'shared-layout/src/shared/layout.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    LayoutModule,
     BrowserModule
   ],
   providers: [],
